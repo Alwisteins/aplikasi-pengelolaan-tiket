b60ffd8892b2e365148c034552413f2f25a153c9
unknown <alwyyourbae@gmail.com>
2024-05-23 06:59:45 +0700

    add sidebar

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "../components/sidebar/Sidebar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main>{children}</main>
+    </div>
+  );
+}
