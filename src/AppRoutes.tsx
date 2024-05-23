6a1f23ec3d305719ee7f3e13369f621baf722626
unknown <alwyyourbae@gmail.com>
2024-05-23 07:03:11 +0700

    add routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
+import Overview from "./pages/overview/Overview";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/overview" element={<Overview />} />
     </Routes>
   );
 }
