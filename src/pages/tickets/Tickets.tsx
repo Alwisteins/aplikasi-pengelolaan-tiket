e4069fbbc28c32df3f07c5f1fea17a46474d7adf
unknown <alwyyourbae@gmail.com>
2024-05-25 22:46:24 +0700

    add topbar & table

--- src/pages/tickets/Tickets.tsx
+++ src/pages/tickets/Tickets.tsx
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Topbar from "../../components/topbar/Topbar";
+import TicketTable from "../../components/table/Table";
+
+interface Ticket {
+  profilePict: string;
+  ticketName: string;
+  ticketUpdatedAt: string;
+  customerName: string;
+  customerCreatedAt: string;
+  ticketDate: string;
+  ticketPriority: string;
+}
+
+export type Tickets = Ticket[];
+
 export default function Tickets() {
+  const [tickets, setTickets] = useState<Tickets | null>(null);
+  const url = "http://localhost:3000/tickets";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setTickets(response.data);
+    });
+  });
+
   return (
-    <div>Tickets</div>
-  )
+    <div className="p-5 h-full space-y-12 bg-[#f7fafe]">
+      <Topbar page="Tickets" />
+      <TicketTable tickets={tickets} />
+    </div>
+  );
 }
