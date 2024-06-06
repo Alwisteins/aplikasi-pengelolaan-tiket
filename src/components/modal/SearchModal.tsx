0ea41931a4bab63fe0a99d58b6c5430423a484c3
unknown <alwyyourbae@gmail.com>
2024-06-06 16:25:58 +0700

    add single responsibility principles

--- src/components/modal/SearchModal.tsx
+++ src/components/modal/SearchModal.tsx
@@ -1,61 +1,47 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { Task } from "../task/Task";
-import { generateStyle } from "../task/Task";
+import { useTasks } from "../../hooks/useTasks";
+import { Task } from "../../types/task";
+import taskStatusStyle from "../../utils/taskStatusStyle";
 import { Icon } from "react-icons-kit";
 import { search } from "react-icons-kit/fa/search";
 
-interface FetchTasksParams {
-  url: string;
-  setTasks: (tasks: Task[]) => void;
-  setFilteredTasks: (task: Task[]) => void;
-}
-
-const fetchTasks = async ({
-  url,
-  setTasks,
-  setFilteredTasks,
-}: FetchTasksParams) => {
-  try {
-    const response = await axios.get(url);
-    const tasks = response.data as Task[];
-    setTasks(tasks);
-    setFilteredTasks(tasks);
-  } catch (err) {
-    console.error("Error fetching tasks:", err);
-  }
-};
-
-const filterTasks = (tasks: Task[] | null, query: string): Task[] | null => {
-  if (!tasks) return null;
-  if (!query.trim()) return tasks;
-  return tasks.filter((task) =>
-    task.name.toLowerCase().includes(query.toLowerCase())
-  );
-};
-
 export default function SearchModal() {
+  const { tasks } = useTasks();
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState("");
-  const [tasks, setTasks] = useState<Task[] | null>(null);
   const [filteredTasks, setFilteredTasks] = useState<Task[] | null>(null);
-  const url = "http://localhost:3000/tasks";
 
   useEffect(() => {
-    fetchTasks({ url, setTasks, setFilteredTasks });
-  }, []);
+    setFilteredTasks(tasks);
+  }, [tasks]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setQuery(value);
-    setFilteredTasks(filterTasks(tasks, value));
+
+    if (!tasks) return;
+
+    const filtered = value.trim()
+      ? tasks.filter((task) =>
+          task.name.toLowerCase().includes(query.toLowerCase())
+        )
+      : tasks;
+
+    setFilteredTasks(filtered);
   };
 
   return (
     <div className="z-10">
       <div className={open ? "block" : "hidden"}>
         <div className="absolute inset-0 bg-black/20 flex items-center justify-center">
-          <div className="relative px-5 py-8 rounded-md space-y-5 bg-white">
+          <div className="relative px-5 pt-14 pb-10 rounded-md space-y-3 bg-white">
+            <button
+              type="button"
+              className="font-bold text-xl absolute top-5 right-5"
+              onClick={() => setOpen(false)}
+            >
+              x
+            </button>
             <input
               type="search"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
@@ -72,7 +58,7 @@ export default function SearchModal() {
                 >
                   <p className="font-medium">{task.name}</p>
                   <p
-                    className={`${generateStyle(
+                    className={`${taskStatusStyle(
                       task.status
                     )} text-xs py-1 px-2 rounded-lg`}
                   >
@@ -80,9 +66,7 @@ export default function SearchModal() {
                   </p>
                 </div>
               ))}
-            <div className="flex justify-center font-bold text-lg">
-              <button onClick={() => setOpen(false)}>x</button>
-            </div>
+            
           </div>
         </div>
       </div>
