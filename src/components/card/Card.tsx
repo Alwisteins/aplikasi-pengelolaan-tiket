93efd4b282e9a8543d752d0c5d7bd21b4655e21f
unknown <alwyyourbae@gmail.com>
2024-06-07 09:09:01 +0700

    remove unused component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,28 +1,17 @@
 interface CardProps {
   title: string;
   total: number | string | undefined;
+  AdditionalStyle?: string
 }
 
-export function Card({ title, total }: CardProps) {
+export default function Card(props: CardProps) {
+  const { title, total, AdditionalStyle } = props
   return (
-    <div className="flex flex-col items-center justify-center h-32 w-60 bg-white rounded-lg hover:border-2 hover:border-blue-700 group">
+    <div className={`${AdditionalStyle} flex flex-col items-center justify-center bg-white rounded-lg hover:border-2 hover:border-blue-700 group`}>
       <p className="text-lg font-semibold text-slate-400 group-hover:text-blue-700">
         {title}
       </p>
       <h1 className="text-2xl font-bold group-hover:text-blue-700">
-        {total ? total : 0}
-      </h1>
-    </div>
-  );
-}
-
-export function CardLong({ title, total }: CardProps) {
-  return (
-    <div className="flex flex-col items-center justify-center w-72 py-5 bg-white rounded-lg hover:border-2 hover:border-blue-700 group">
-      <p className="font-semibold text-slate-400 group-hover:text-blue-700">
-        {title}
-      </p>
-      <h1 className="text-xl font-bold group-hover:text-blue-700">
         {total ? total : 0}
       </h1>
     </div>
